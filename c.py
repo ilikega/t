b7759618f812053c252a7238c66700f184e66e8a
王仔仔 <54138747+ilikega@users.noreply.github.com>
2019-09-11 11:27:10 +0800

    Rename a.py to c.py

---
+++ c.py
@@ -0,0 +1,10 @@
+from sko.GA import GA
+
+
+def demo_func(x):
+    x1, x2, x3 = x
+    return x1 ** 2 + (x2 - 0.05) ** 2 + x3 ** 2
+
+
+ga = GA(func=demo_func, lb=[-1, -10, -5], ub=[2, 10, 2], max_iter=500)
+best_x, best_y = ga.fit()
